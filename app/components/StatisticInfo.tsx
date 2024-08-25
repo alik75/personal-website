caa920614e72c26df349eb0812e23632ba199fb1
alikatiraie <alikatiraie96@gmail.com>
2024-08-25 11:25:45 +0400

    fix(eslint): solve key on map iteration

--- app/components/StatisticInfo.tsx
+++ app/components/StatisticInfo.tsx
@@ -15,8 +15,8 @@ const StatisticInfo = ({ items, rtl = false  }: Props) => {
 
   return (
     <div className="flex flex-col md:gap-2 gap-1">
-      {items.map((item, idx) => (
-        <span className={`flex ${rtl?"flex-row-reverse":"flex-row"} md:gap-2 gap-1 items-center`}>
+      {items.map((item:StatisticItem, idx:number) => (
+        <span key={idx} className={`flex ${rtl?"flex-row-reverse":"flex-row"} md:gap-2 gap-1 items-center`}>
           <span className="md:text-sm sm:text-xs text-[.5rem] text-yellow">{item.key}</span>
           <small className="text-yellow">~</small>
           <span className="md:text-sm sm:text-xs text-[.5rem] font-bold text-white">{item.value}</span>
